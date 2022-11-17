21cc965643121631bdc28f3cf6a1172ff4ed6435
uzoma <chidumaga@gmail.com>
2022-11-17 16:14:14 +0100

    loads in background from contentful backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import helpers from './utils/helpers'
 
 const App = () => {
+
+  const [bgImage, setBgImage] = useState(null)
+
+  const query = `
+    query {
+      assetCollection {
+        items {
+          url
+        }
+      }
+    }
+  `
+
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/lc8eccgjroyj/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: "Bearer MYs2y3N2wGIn6LTILCGIKWxr31-RKCXTkYbW_qP7Llg",
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        // rerender the entire component with new data
+        setBgImage(helpers.getRandomItem(data.assetCollection.items).url);
+      });
+  }, [query]);
+  
   return (
-    <h1>Briqolage</h1>
+    <div id="background" style={{backgroundImage: `url(${bgImage})`}}></div>
   )
 }
 
